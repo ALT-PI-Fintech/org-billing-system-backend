345a3d438cc55d4cd91d690734f348e13b0be3da
Ayan <ayanshaikh.altpi@gmail.com>
2024-06-20 15:20:08 +0530

    Given Api checked

--- app/controllers/agent/allAgentDetails.controller.js
+++ app/controllers/agent/allAgentDetails.controller.js
@@ -6,12 +6,13 @@ export const allAgentDetails = async (req, res) => {
     const agentDetails = await agent_details.findAll({
       attributes: ["agent_id", "agent_name", "agent_type"],
     });
+    console.log(agentDetails);
     return res
       .status(200)
       .json({ success: true, message: "all Agents", data: { agentDetails } });
   } catch (error) {
     return res
       .status(404)
-      .json({ success: false, message: "internal error", data: null });
+      .json({ success: false, message: error, data: null });
   }
 };
